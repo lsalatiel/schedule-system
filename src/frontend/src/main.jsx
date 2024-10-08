7cede10ce5514bcaf48fb8f2cc7ac4bf781ea2ed
lsalatiel <lucassalatiel1004@gmail.com>
2024-10-08 16:50:56 -0300

    feat: set up routes and views

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+
+import Root from "./views/Root.tsx";
+import Register from "./views/Register.tsx";
+import AdminLogin from "./views/AdminLogin.tsx";
+import Admin from "./views/Admin.tsx";
 import "./index.css";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Root />,
+    },
+    {
+        path: "/cadastro",
+        element: <Register />,
+    },
+    {
+        path: "/adm-login",
+        element: <AdminLogin />,
+    },
+    {
+        path: "/admin",
+        element: <Admin />,
+    },
+]);
+
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <App />
-    </StrictMode>
+    <RouterProvider router={router} />
 );
+
