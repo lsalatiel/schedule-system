f6e63ef9011817078c5ee2d43d6f8f69337047d8
lsalatiel <lucassalatiel1004@gmail.com>
2024-10-11 14:23:46 -0300

    feat: add root view

--- src/frontend/src/views/Root.tsx
+++ src/frontend/src/views/Root.tsx
@@ -1,9 +1,55 @@
+import { useState } from "react";
 import React from "react";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/style.css";
+import { ptBR } from "date-fns/locale";
+import "./root.css";
 
 export default function Root() {
+    const [selectedDate, setSelectedDate] = useState<Date | undefined>();
+    const [selectedTime, setSelectedTime] = useState<string | undefined>();
+
+    const availableTimes = [
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00"
+    ]
+
     return (
-        <div>
-            <h1>root</h1>
+        <div className="calendar-container">
+            <div className="calendar-inside-container">
+                <DayPicker
+                    locale={ptBR}
+                    mode="single"
+                    selected={selectedDate}
+                    onSelect={setSelectedDate}
+                />
+            </div>
+
+            {selectedDate && (
+                <div className="time-selection">
+                    <h3>Selecione um horário:</h3>
+                    <ul className="time-options">
+                        {availableTimes.map((time) => (
+                            <li key={time}>
+                                <button
+                                    className={`time-button ${selectedTime === time ? "selected" : ""}`}
+                                    onClick={() => setSelectedTime(time)}
+                                >
+                                    {time}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
         </div>
     );
 }
