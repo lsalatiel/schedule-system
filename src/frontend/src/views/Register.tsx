8f2f6d15911d6c6d300b90a130a817b75186797e
lsalatiel <lucassalatiel1004@gmail.com>
2024-10-14 16:17:57 -0300

    feat: add register view

--- src/frontend/src/views/Register.tsx
+++ src/frontend/src/views/Register.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 
 export default function Register() {
+    const selectedDate = localStorage.getItem("selectedDate");
+    const selectedTime = localStorage.getItem("selectedTime");
+
     return (
         <div>
+            <div className="date-info-container">
+                <h2>{selectedDate}</h2>
+                <h2>{selectedTime}</h2>
+            </div>
+            <div className="inputs-container">
+                <h3>Por favor, preencha os campos abaixo:</h3>
+                <h4>Você receberá uma mensagem de confirmação em seu Whatsapp.</h4>
+                <input type="text" placeholder="Nome" />
+                <input type="text" placeholder="E-mail" />
+                <input type="text" placeholder="Telefone" />
+                <button>Finalizar agendamento</button>
+            </div>
         </div>
     );
 }
