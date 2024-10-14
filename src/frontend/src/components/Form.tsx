19e0bbb6781ff97813097f1f43cc70ac5d199e31
lsalatiel <lucassalatiel1004@gmail.com>
2024-10-14 20:17:46 -0300

    feat: add form component

---
+++ src/frontend/src/components/Form.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import "./styles/form.css";
+
+export default function Form() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phone, setPhone] = useState("");
+
+    // Handler functions for input changes
+    const handleNameChange = (e:any) => {
+        setName(e.target.value);
+    };
+
+    const handleEmailChange = (e:any) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePhoneChange = (e:any) => {
+        setPhone(e.target.value);
+    };
+
+    // Function to handle form submission or accessing the values
+    const handleSubmit = (e:any) => {
+        e.preventDefault();
+        console.log('Name:', name);
+        console.log('Email:', email);
+        console.log('Phone:', phone);
+    };
+
+    return (
+        <div>
+            <form className="user-form" onSubmit={handleSubmit}>
+                <input
+                    className="user-input"
+                    type="text"
+                    placeholder="Nome"
+                    value={name}
+                    onChange={handleNameChange}
+                />
+                <input
+                    className="user-input"
+                    type="text"
+                    placeholder="E-mail"
+                    value={email}
+                    onChange={handleEmailChange}
+                />
+                <input
+                    className="user-input"
+                    type="text"
+                    placeholder="Telefone"
+                    value={phone}
+                    onChange={handlePhoneChange}
+                />
+                <button className="submit-button" type="submit">Submit</button>
+            </form>
+        </div>
+    );
+}
--- src/frontend/src/views/Register.tsx
+++ src/frontend/src/views/Register.tsx
@@ -1,22 +1,21 @@
 import React from "react";
+import "./styles/register.css";
+import Form from "../components/Form.tsx";
 
 export default function Register() {
     const selectedDate = localStorage.getItem("selectedDate");
     const selectedTime = localStorage.getItem("selectedTime");
 
     return (
-        <div>
+        <div className="register-content">
             <div className="date-info-container">
                 <h2>{selectedDate}</h2>
                 <h2>{selectedTime}</h2>
             </div>
-            <div className="inputs-container">
+            <div className="form-container">
                 <h3>Por favor, preencha os campos abaixo:</h3>
                 <h4>Você receberá uma mensagem de confirmação em seu Whatsapp.</h4>
-                <input type="text" placeholder="Nome" />
-                <input type="text" placeholder="E-mail" />
-                <input type="text" placeholder="Telefone" />
-                <button>Finalizar agendamento</button>
+                <Form />
             </div>
         </div>
     );
